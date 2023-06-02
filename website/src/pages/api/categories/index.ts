a9256045eb8131ae08398d50e3ae4248e217ae6d
vasingh <vasingh@nvidia.com>
2023-06-02 16:48:46 -0400

    minor updates to fetch categories

--- website/src/pages/api/categories/index.ts
+++ website/src/pages/api/categories/index.ts
@@ -5,9 +5,8 @@ import { createApiClient } from "src/lib/oasst_client_factory";
 const handler = withoutRole("banned", async (req, res, token) => {
   const client = await createApiClient(token);
   const userLanguage = getLanguageFromRequest(req);
-  const page = parseInt(req.query.page as string, 10) || 1;
-  const messages = await client.fetch_recent_messages(userLanguage, page);
-  res.status(200).json(messages["messages"]);
+  const categories = await client.fetch_categories(userLanguage);
+  res.status(200).json(categories);
 });
 
 export default handler;
